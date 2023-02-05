1aa6b9401cfa2b78a456245edd8e4d17a5bbd6e5
Travis Menghini <menghinitravis@gmail.com>
2023-02-04 21:31:31 -0600

    Can now read a database file and the table "Athlete"

--- src/App.js
+++ src/App.js
@@ -1,22 +1,46 @@
+import React, { useState } from 'react';
 import logo from './logo.svg';
+import MDBReader from "mdb-reader";
 import './App.css';
 
+if (typeof Buffer === 'undefined') {
+  global.Buffer = require('buffer/').Buffer;
+}
+
+
 function App() {
+  const [tableData, setTableData] = useState([]);
+  const [fileName, setFileName] = useState('');
+
+  const handleFileDrop = (event) => {
+    event.preventDefault();
+    const file = event.dataTransfer.files[0];
+    setFileName(file.name);
+
+    const reader = new FileReader();
+    reader.onload = (event) => {
+      const buffer = Buffer.from(event.target.result);
+      const mdbReader = new MDBReader(buffer);
+      const table = mdbReader.getTable("Athlete");
+      setTableData(table.getData());
+    };
+    reader.readAsArrayBuffer(file);
+  };
+
   return (
-    <div className="App">
+    <div
+      className="App"
+      onDrop={handleFileDrop}
+      onDragOver={(event) => event.preventDefault()}
+    >
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {fileName ? `Table data for ${fileName}:` : 'Drop a file to display table data'}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {tableData.map((row) => (
+          <div key={row.id}>{JSON.stringify(row)}</div>
+        ))}
       </header>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -3,18 +3,6 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { readFileSync } from "fs";
-import MDBReader from "mdb-reader";
-
-//Example
-const buffer = readFileSync("database.mdb");
-const reader = new MDBReader(buffer);
-
-reader.getTableNames(); // ['Cats', 'Dogs', 'Cars']
-
-const table = reader.getTable("Cats");
-table.getColumnNames(); // ['id', 'name', 'color']
-table.getData(); // [{id: 5, name: 'Ashley', color: 'black'}, ...]
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
