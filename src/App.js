be26134792aaffdfdb515c7b0957380c2e1b3e77
Travis Menghini <menghinitravis@gmail.com>
2023-02-08 21:30:59 -0600

    Split App.js into two files. App.js and MeetTable.js

--- src/App.js
+++ src/App.js
@@ -1,126 +1,17 @@
 import React, { useState } from 'react';
 import logo from './logo.svg';
-import MDBReader from "mdb-reader";
 import './App.css';
-import {
-  Table,
-  TableBody,
-  TableCell,
-  TableHead,
-  TableRow,
-  Paper,
-} from "@mui/material/";
+import MeetTable from './MeetTable';
 
-if (typeof Buffer === 'undefined') {
-  global.Buffer = require('buffer/').Buffer;
-}
 
 
 function App() {
-  const [tableData, setTableData] = useState([]);
-  const [fileName, setFileName] = useState('');
 
-  const requiredTables = [
-    "AGEGROUPS",
-    "AthInfo",
-    "Athlete",
-    "ATHRECR",
-    "ATHREG",
-    "BRACKET",
-    "COACHES",
-    "CODE",
-    "CustomLayout",
-    "CustomLayoutFields",
-    "CustomLayoutValues",
-    "CUSTOMRPTS",
-    "ENTRY",
-    "ESPLITS",
-    "FAVORITES",
-    "JOURNAL",
-    "MEET",
-    "MTEVENT",
-    "MTEVENTE",
-    "OPTIONS",
-    "RECNAME",
-    "RECORDS",
-    "RELAY",
-    "REPORTORDER",
-    "RESULT",
-    "SPLITS",
-    "STDNAME",
-    "TEAM",
-    "TMREG"
-  ];
-
-  const handleFileDrop = (event) => {
-    event.preventDefault();
-    const file = event.dataTransfer.files[0];
-    setFileName(file.name);
-
-    const reader = new FileReader();
-    reader.onload = (event) => {
-
-      try {
-        const buffer = Buffer.from(event.target.result);
-        const mdbReader = new MDBReader(buffer);
-        let tables = mdbReader.getTableNames();
-
-        let containsAllTables = true;
-        for (let i = 0; i < requiredTables.length; i++) {
-          if (!tables.includes(requiredTables[i])) {
-            containsAllTables = false;
-            break;
-          }
-        }
-
-        if (containsAllTables) {
-          const athleteTable = mdbReader.getTable("Athlete");
-          const meetTable = mdbReader.getTable("MEET");
-          const resultsTable = mdbReader.getTable("RESULT");
-          setTableData(meetTable.getData());
-        } else {
-          console.log("This is a database file, but it doesn't appear to be from HYTEK Track and Field Manager");
-          setTableData(["This is a database file, but it doesn't appear to be from HYTEK Track and Field Manager"]);
-        }
-      } catch (error) {
-        //console.error(error);
-        setTableData([]);
-        setFileName("This is not a database file, nor does it appear to be from HYTEK Track and Field Manager");
-      }
-    };
-    reader.readAsArrayBuffer(file);
-  };
   return (
-    <div className="App" onDrop={handleFileDrop} onDragOver={(event) => event.preventDefault()}>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          {fileName ? `Table data for ${fileName}:` : "Drop a file to display table data"}
-        </p>
-        <Paper>
-          <Table>
-            <TableHead>
-              <TableRow>
-                <TableCell>ID</TableCell>
-                <TableCell>MEET</TableCell>
-                <TableCell>DATE</TableCell>
-                <TableCell>LOCATION</TableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {tableData.map((row, index) => (
-                <TableRow key={row.MEET || index}>
-                  <TableCell>{row.MEET}</TableCell>
-                  <TableCell>{row.MNAME}</TableCell>
-                  <TableCell>{row.START.toDateString()}</TableCell>
-                  <TableCell>{row.LOCATION}</TableCell>
-                </TableRow>
-              ))}
-            </TableBody>
-          </Table>
-        </Paper>
+        <h1>HYTEK Track & Field Database Display</h1>
+        <MeetTable />
       </header>
-    </div>
   );
 }
 
