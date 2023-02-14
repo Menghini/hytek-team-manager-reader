49835725c23ee07c0405f7386fcb80fb10a9795c
Travis Menghini <menghinitravis@gmail.com>
2023-02-13 19:44:56 -0600

    Packed together a convertRawScoreToMark function

--- src/MeetTable.js
+++ src/MeetTable.js
@@ -130,57 +130,61 @@ function MeetTable() {
         setOpen(false);
     };
 
+    const convertRawScoreToMark = (score, MARK_YD) => {
+        //Score is the raw score from the database.
+        //MARK_YD is either E for imperial or M for metric.
+        let mark, convert, rawMetric;
+        if (score === 0) {
+            mark = "F";
+        }
+        else if (score < 0 && MARK_YD === "E") {
+            //Imperial Scored Field Event
+            const feet = Math.floor(score * -1 / 100) / 12;
+            const inches = (score * -1 / 100) % 12;
+            convert = ((feet * 12 + inches) * 0.0254).toFixed(2) + "m";
+            rawMetric = ((feet * 12 + inches) * 0.0254);
+            let inchString = inches.toFixed(2);
+            if (inchString.length === 4) {
+                inchString = "0" + inchString;
+            }
+            mark = `${feet.toFixed(0)}-${inchString}`;
+        } else if (score < 0 && MARK_YD === "M") {
+            //Metric Scored Field Event
+            const meters = score * -1 / 100;
+            rawMetric = meters;
+            mark = `${meters.toFixed(2)}m`;
+            let feet = meters * 3.28084;
+            let inches = (feet - Math.floor(feet)) * 12;
+            if (inches === 12.00) {
+                feet += 1;
+                inches = 0;
+            } else {
+                inches = Math.round(inches / 0.25) * 0.25; // round to nearest quarter inch
+            }
+            convert = `${Math.floor(feet).toFixed(0)}-${inches.toFixed(2)}`;
+
+        } else {
+            //Timed event
+            const scoreInSeconds = score / 100;
+            const minutes = Math.floor(scoreInSeconds / 60);
+            const seconds = Math.floor(scoreInSeconds % 60);
+            const milliseconds = score % 100;
+            const showMinutes = minutes >= 1;
+            mark = `${showMinutes ? `${minutes}:` : ''}${String(seconds).padStart(2, '0')}.${String(milliseconds).padStart(2, '0')}`;
+        }
+        return {
+            mark: mark, //Used to denote the result in which it was measured in
+            convert: convert, //Used to denote the result in which it was converted to
+            rawMetric: rawMetric //Use to help with calculating improvement
+        };
+    }
+
     const handleSelectionChange = (newSelection) => {
-        console.log("athletesTable:", athletesTable);
         const selectedMeetId = newSelection[0];
         const selectedMeetRows = resultsTable.getData()
             .filter(row => row.MEET === selectedMeetId)
             .map((row, index) => {
-                const scoreInSeconds = row.SCORE / 100;
-                const minutes = Math.floor(scoreInSeconds / 60);
-                const seconds = Math.floor(scoreInSeconds % 60);
-                const milliseconds = row.SCORE % 100;
-                const showMinutes = minutes >= 1;
-                let mark;
-                let convert;
-                if (row.SCORE === 0) {
-                    mark = "F";
-                }
-                else if (row.SCORE < 0 && row.MARK_YD === "E") {
-                    //Imperial Scored Field Event
-                    const feet = Math.floor(row.SCORE * -1 / 100) / 12;
-                    const inches = (row.SCORE * -1 / 100) % 12;
-                    convert = ((feet * 12 + inches) * 0.0254).toFixed(2) + "m";
-                    let inchString = inches.toFixed(2);
-                    if (inchString.length === 4) {
-                        inchString = "0" + inchString;
-                    }
-                    mark = `${feet.toFixed(0)}-${inchString}`;
-                } else if (row.SCORE < 0 && row.MARK_YD === "M") {
-                    //Metric Scored Field Event
-                    const meters = row.SCORE * -1 / 100;
-                    mark = `${meters.toFixed(2)}m`;
-                    let feet = meters * 3.28084;
-                    let inches = (feet - Math.floor(feet)) * 12;
-                    if (inches === 12.00) {
-                        feet += 1;
-                        inches = 0;
-                    } else {
-                        inches = Math.round(inches / 0.25) * 0.25; // round to nearest quarter inch
-                    }
-                    convert = `${Math.floor(feet).toFixed(0)}-${inches.toFixed(2)}`;
-
-                } else {
-                    //Timed event
-                    const scoreInSeconds = row.SCORE / 100;
-                    const minutes = Math.floor(scoreInSeconds / 60);
-                    const seconds = Math.floor(scoreInSeconds % 60);
-                    const milliseconds = row.SCORE % 100;
-                    const showMinutes = minutes >= 1;
-                    mark = `${showMinutes ? `${minutes}:` : ''}${String(seconds).padStart(2, '0')}.${String(milliseconds).padStart(2, '0')}`;
-                }
-
-
+                const { mark, convert } = convertRawScoreToMark(row.SCORE, row.MARK_YD);
                 let eventName = '';
                 let eventType;
                 switch (row.I_R) {
@@ -253,11 +257,8 @@ function MeetTable() {
                     //ATHLETE: athletesTable && athletesTable.find(athlete => athlete.Athlete === row.ATHLETE)?.Last + ', ' + athletesTable.find(athlete => athlete.Athlete === row.ATHLETE)?.First,
                     FIRST: athletesTable && athletesTable.find(athlete => athlete.Athlete === row.ATHLETE)?.First,
                     LAST: athletesTable && athletesTable.find(athlete => athlete.Athlete === row.ATHLETE)?.Last,
-                    ATHLETE: row.ATHLETE,
+                    ATHLETEID: row.ATHLETE,
                     EVENTNAME: eventName,
-                    MINUTES: minutes,
-                    SECONDS: seconds,
-                    MILLISECONDS: milliseconds,
                     SCORE: mark,
                     CONVERT: convert,
                     RESULT: row.RESULT,
@@ -274,7 +275,7 @@ function MeetTable() {
     const resultsTableColumns = [
         { field: 'LAST', headerName: 'Last Name', flex: 1 },
         { field: 'FIRST', headerName: 'First Name', flex: 1 },
-        { field: 'ATHLETE', headerName: 'Athlete ID', flex: 1 },
+        { field: 'ATHLETEID', headerName: 'Athlete ID', flex: 1 },
         { field: 'EVENTTYPE', headerName: 'Event Type', flex: 1 },
         { field: 'EVENTNAME', headerName: 'Event Name', flex: 1 },
         { field: 'SCORE', headerName: 'Mark', flex: 1 },
